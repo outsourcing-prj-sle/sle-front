32cc4d212aa6ed4278234dc110ae426e2a76cb9e
hwansoo <realyhs1@gmail.com>
2024-06-13 10:28:24 +0900

    상단 alert창

---
+++ src/components/AppTopAlert.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="top-10 fixed py-1 px-2 opacity-60 rounded-md z-50 bg-black text-white"
+    style="left: 50%; transform: translateX(-50%)"
+    v-if="showAlert && text"
+  >
+    {{ text }}
+  </div>
+  <!-- </div> -->
+</template>
+
+<script>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+export default {
+  props: {
+    showAlert: {
+      type: Boolean,
+      default: false,
+    },
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+  setup() {
+    const route = useRoute();
+
+    return {};
+  },
+};
+</script>
+
+<style>
+/* Footer 스타일 */
+</style>
