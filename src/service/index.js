9aaf79481a2a9e29285d70ac8dba6ebf51ae8f63
hwansoo <realyhs1@gmail.com>
2024-06-14 17:03:41 +0900

    apiClient 정의

---
+++ src/service/index.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: 'https://domain/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+apiClient.interceptors.request.use(
+  (config) => {
+    // const currentUserId =
+    // config.headers.Authorization = currentUserId
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    // 오류 응답을 처리
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
