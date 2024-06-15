ce243909627fd1c4ad2ee39c9970f29ef0414799
chw970320 <chw970320@gmail.com>
2024-06-15 23:20:28 +0900

    Feat: CardSlider 추가  - 컴포넌트화 및 CardSlider 로직 추가

---
+++ src/components/AppCardItem.vue
@@ -0,0 +1,345 @@
+<template>
+    <div v-if="type === 1"
+        class="flex gap-4 justify-between mt-6 text-base font-medium text-neutral-700 max-md:flex-wrap"
+      >
+        <div @click="clickPrev"
+          class="justify-center flex items-center px-5 my-auto bg-black rounded-full h-[55px] w-[55px] max-md:pr-5"
+        >
+          <img
+            src="./../assets/img/left_arrow.png"
+            alt="left_arrow1"
+            class="shrink-0 max-w-full"
+          />
+        </div>
+
+        <div ref="cardSlider"
+          class="flex flex-1 gap-4 mt-6 text-base font-medium text-neutral-700 max-md:flex-wrap overflow-scroll no-scrollbar"
+        >
+          <article
+            class="flex flex-col relative pt-10 px-6 bg-white rounded-xl border-2 border-blue-500 border-solid pb-[124px]"
+          >
+            <div class="self-center text-xl font-bold">마음알기 설문1</div>
+            <div class="mt-8 max-md:mr-2.5">2023. 09. 01 ~ 12. 01</div>
+            <div class="self-center mt-3.5 text-red-600">D-70</div>
+            <div class="w-[206px] flex gap-1.5">
+              <button
+                class="items-center flex-1 px-2 py-2.5 mt-8 text-white whitespace-nowrap bg-blue-500 rounded-[30px] max-md:px-5 max-md:mr-2.5 overflow-hidden text-ellipsis"
+                v-if="loginType === 'student'"
+                @click="() => goReportNotice(1)"
+              >
+                시작하기
+              </button>
+              <button
+                class="items-center flex-1 px-2 py-2.5 mt-8 text-white whitespace-nowrap bg-blue-500 rounded-[30px] max-md:px-5 max-md:mr-2.5 overflow-hidden text-ellipsis"
+                v-if="loginType === 'teacher'"
+                @click="() => copyURL(1)"
+              >
+                URL 복사
+              </button>
+              <button
+                class="items-center flex-1 px-2 py-2.5 mt-8 text-white whitespace-nowrap bg-blue-700 rounded-[30px] max-md:px-5 max-md:mr-2.5 overflow-hidden text-ellipsis"
+                v-if="loginType === 'teacher'"
+                @click="() => openQRCodePopup(1)"
+              >
+                QR 코드
+              </button>
+            </div>
+            <img
+              src="./../assets/img/person_icn.png"
+              alt="person_icn"
+              class="self-end max-w-full aspect-[1.28] w-[116px] absolute bottom-0 right-1.5"
+            />
+          </article>
+          <article
+            class="flex flex-col relative pt-10 px-6 bg-white rounded-xl border-2 border-blue-500 border-solid pb-[124px]"
+          >
+            <div class="self-center text-xl font-bold">마음알기 설문2</div>
+            <div class="mt-8 max-md:mr-2.5">2023. 09. 01 ~ 12. 01</div>
+            <div class="self-center mt-3.5 text-red-600">D-70</div>
+            <div class="w-[206px] flex gap-1.5">
+              <button
+                class="items-center flex-1 px-2 py-2.5 mt-8 text-white whitespace-nowrap bg-blue-700 rounded-[30px] max-md:px-5 max-md:mr-2.5 overflow-hidden text-ellipsis"
+                v-if="loginType === 'student'"
+              >
+                이어서 진행하기
+              </button>
+              <button
+                class="items-center flex-1 px-2 py-2.5 mt-8 text-white whitespace-nowrap bg-blue-500 rounded-[30px] max-md:px-5 max-md:mr-2.5 overflow-hidden text-ellipsis"
+                v-if="loginType === 'teacher'"
+              >
+                URL 복사
+              </button>
+              <button
+                class="items-center flex-1 px-2 py-2.5 mt-8 text-white whitespace-nowrap bg-blue-700 rounded-[30px] max-md:px-5 max-md:mr-2.5 overflow-hidden text-ellipsis"
+                v-if="loginType === 'teacher'"
+                @click="openQRCodePopup"
+              >
+                QR 코드
+              </button>
+            </div>
+            <img
+              src="./../assets/img/person_icn.png"
+              alt="person_icn"
+              class="self-end max-w-full aspect-[1.28] w-[116px] absolute bottom-0 right-1.5"
+            />
+          </article>
+          <article
+            class="flex flex-col relative pt-10 px-6 bg-white rounded-xl border-2 border-blue-500 border-solid pb-[124px]"
+          >
+            <div class="self-center text-xl font-bold">마음알기 설문3</div>
+            <div class="mt-8 max-md:mr-2.5">2023. 09. 01 ~ 12. 01</div>
+            <div class="self-center mt-3.5 text-red-600">D-70</div>
+            <div class="w-[206px] flex gap-1.5">
+              <button
+                class="items-center flex-1 px-2 py-2.5 mt-8 text-white whitespace-nowrap bg-blue-700 rounded-[30px] max-md:px-5 max-md:mr-2.5 overflow-hidden text-ellipsis"
+                v-if="loginType === 'student'"
+              >
+                이어서 진행하기
+              </button>
+              <button
+                class="items-center flex-1 px-2 py-2.5 mt-8 text-white whitespace-nowrap bg-blue-500 rounded-[30px] max-md:px-5 max-md:mr-2.5 overflow-hidden text-ellipsis"
+                v-if="loginType === 'teacher'"
+              >
+                URL 복사
+              </button>
+              <button
+                class="items-center flex-1 px-2 py-2.5 mt-8 text-white whitespace-nowrap bg-blue-700 rounded-[30px] max-md:px-5 max-md:mr-2.5 overflow-hidden text-ellipsis"
+                v-if="loginType === 'teacher'"
+                @click="openQRCodePopup"
+              >
+                QR 코드
+              </button>
+            </div>
+            <img
+              src="./../assets/img/person_icn.png"
+              alt="person_icn"
+              class="self-end max-w-full aspect-[1.28] w-[116px] absolute bottom-0 right-1.5"
+            />
+          </article>
+          <article
+            class="flex flex-col relative pt-10 px-6 bg-white rounded-xl border-2 border-blue-500 border-solid pb-[124px]"
+          >
+            <div class="self-center text-xl font-bold">마음알기 설문4</div>
+            <div class="mt-8 max-md:mr-2.5">2023. 09. 01 ~ 12. 01</div>
+            <div class="self-center mt-3.5 text-red-600">D-70</div>
+            <div class="w-[206px] flex gap-1.5">
+              <button
+                class="items-center flex-1 px-2 py-2.5 mt-8 text-white whitespace-nowrap bg-blue-700 rounded-[30px] max-md:px-5 max-md:mr-2.5 overflow-hidden text-ellipsis"
+                v-if="loginType === 'student'"
+              >
+                이어서 진행하기
+              </button>
+              <button
+                class="items-center flex-1 px-2 py-2.5 mt-8 text-white whitespace-nowrap bg-blue-500 rounded-[30px] max-md:px-5 max-md:mr-2.5 overflow-hidden text-ellipsis"
+                v-if="loginType === 'teacher'"
+              >
+                URL 복사
+              </button>
+              <button
+                class="items-center flex-1 px-2 py-2.5 mt-8 text-white whitespace-nowrap bg-blue-700 rounded-[30px] max-md:px-5 max-md:mr-2.5 overflow-hidden text-ellipsis"
+                v-if="loginType === 'teacher'"
+                @click="openQRCodePopup"
+              >
+                QR 코드
+              </button>
+            </div>
+            <img
+              src="./../assets/img/person_icn.png"
+              alt="person_icn"
+              class="self-end max-w-full aspect-[1.28] w-[116px] absolute bottom-0 right-1.5"
+            />
+          </article>
+          <article
+            class="flex flex-col relative pt-10 px-6 bg-white rounded-xl border-2 border-blue-500 border-solid pb-[124px]"
+          >
+            <div class="self-center text-xl font-bold">마음알기 설문6</div>
+            <div class="mt-8 max-md:mr-2.5">2023. 09. 01 ~ 12. 01</div>
+            <div class="self-center mt-3.5 text-red-600">D-70</div>
+            <div class="w-[206px] flex gap-1.5">
+              <button
+                class="items-center flex-1 px-2 py-2.5 mt-8 text-white whitespace-nowrap bg-blue-700 rounded-[30px] max-md:px-5 max-md:mr-2.5 overflow-hidden text-ellipsis"
+                v-if="loginType === 'student'"
+              >
+                이어서 진행하기
+              </button>
+              <button
+                class="items-center flex-1 px-2 py-2.5 mt-8 text-white whitespace-nowrap bg-blue-500 rounded-[30px] max-md:px-5 max-md:mr-2.5 overflow-hidden text-ellipsis"
+                v-if="loginType === 'teacher'"
+              >
+                URL 복사
+              </button>
+              <button
+                class="items-center flex-1 px-2 py-2.5 mt-8 text-white whitespace-nowrap bg-blue-700 rounded-[30px] max-md:px-5 max-md:mr-2.5 overflow-hidden text-ellipsis"
+                v-if="loginType === 'teacher'"
+                @click="openQRCodePopup"
+              >
+                QR 코드
+              </button>
+            </div>
+            <img
+              src="./../assets/img/person_icn.png"
+              alt="person_icn"
+              class="self-end max-w-full aspect-[1.28] w-[116px] absolute bottom-0 right-1.5"
+            />
+          </article>
+          <article
+            class="flex flex-col relative pt-10 px-6 bg-white rounded-xl border-2 border-blue-500 border-solid pb-[124px]"
+          >
+            <div class="self-center text-xl font-bold">마음알기 설문7</div>
+            <div class="mt-8 max-md:mr-2.5">2023. 09. 01 ~ 12. 01</div>
+            <div class="self-center mt-3.5 text-red-600">D-70</div>
+            <div class="w-[206px] flex gap-1.5">
+              <button
+                class="items-center flex-1 px-2 py-2.5 mt-8 text-white whitespace-nowrap bg-blue-700 rounded-[30px] max-md:px-5 max-md:mr-2.5 overflow-hidden text-ellipsis"
+                v-if="loginType === 'student'"
+              >
+                이어서 진행하기
+              </button>
+              <button
+                class="items-center flex-1 px-2 py-2.5 mt-8 text-white whitespace-nowrap bg-blue-500 rounded-[30px] max-md:px-5 max-md:mr-2.5 overflow-hidden text-ellipsis"
+                v-if="loginType === 'teacher'"
+              >
+                URL 복사
+              </button>
+              <button
+                class="items-center flex-1 px-2 py-2.5 mt-8 text-white whitespace-nowrap bg-blue-700 rounded-[30px] max-md:px-5 max-md:mr-2.5 overflow-hidden text-ellipsis"
+                v-if="loginType === 'teacher'"
+                @click="openQRCodePopup"
+              >
+                QR 코드
+              </button>
+            </div>
+            <img
+              src="./../assets/img/person_icn.png"
+              alt="person_icn"
+              class="self-end max-w-full aspect-[1.28] w-[116px] absolute bottom-0 right-1.5"
+            />
+          </article>
+        </div>
+
+        <div @click="clickNext"
+          class="justify-center flex items-center px-5 my-auto bg-black rounded-full h-[55px] w-[55px] max-md:pr-5"
+        >
+          <img
+            src="./../assets/img/right_arrow.png"
+            alt="right_arrow1"
+            class="shrink-0 max-w-full"
+          />
+        </div>
+      </div>
+
+      <div v-if="type === 2"
+        class="flex gap-4 justify-between mt-6 text-base font-medium text-neutral-700 max-md:flex-wrap"
+      >
+        <div
+          class="justify-center flex items-center px-5 my-auto bg-black rounded-full h-[55px] w-[55px] max-md:pr-5"
+        >
+          <img
+            src="./../assets/img/left_arrow.png"
+            alt="left_arrow2"
+            class="shrink-0 max-w-full"
+          />
+        </div>
+
+        <div
+          class="flex flex-1 gap-4 mt-6 text-base font-medium text-neutral-700 max-md:flex-wrap overflow-scroll no-scrollbar"
+        >
+          <article
+            class="flex flex-col relative pt-10 px-6 bg-white rounded-xl border-2 border-zinc-600 border-solid pb-[124px]"
+          >
+            <div
+              class="self-center text-xl font-bold text-center text-neutral-700"
+            >
+              마음알기 설문5
+            </div>
+            <div
+              class="mt-8 text-base font-medium text-neutral-700 max-md:mr-2.5"
+            >
+              2023. 09. 01 ~ 12. 01
+            </div>
+            <button
+              class="justify-center w-[206px] items-center px-5 py-3 mt-8 text-white whitespace-nowrap bg-zinc-600 rounded-[30px] max-md:px-5 max-md:mr-2.5 overflow-hidden"
+            >
+              활동 참여가 완료되었어요!
+            </button>
+            <img
+              src="./../assets/img/person_bubble_icn.png"
+              alt="person_icn"
+              class="self-end max-w-full aspect-[1.6] w-[170px] absolute bottom-0 right-1.5"
+            />
+          </article>
+        </div>
+
+        <div
+          class="justify-center flex items-center px-5 my-auto bg-black rounded-full h-[55px] w-[55px] max-md:pr-5"
+        >
+          <img
+            src="./../assets/img/right_arrow.png"
+            alt="right_arrow2"
+            class="shrink-0 max-w-full"
+          />
+        </div>
+      </div>
+  </template>
+  
+  <script>
+  import { ref, onMounted } from 'vue';
+  
+  export default {
+    name: 'AppCardItem',
+    props: {
+        options: {
+            type: String,
+        }
+    },
+    setup(props) {
+        const type = props.options;
+        const cardSlider = ref(null);
+        const articleWidth = ref(0);
+        const nowScrollX = ref(0);
+
+        onMounted(async () => {
+            console.log(type);
+
+            articleWidth.value = document.querySelector('article').offsetWidth;
+        });
+
+        const clickPrev = () => {
+            nowScrollX.value -= (articleWidth.value + 16);
+
+            if(nowScrollX.value < 0) nowScrollX.value = 0;
+
+            cardSlider.value.scrollLeft = nowScrollX.value;
+        };
+
+        const clickNext = () => {
+            if(cardSlider.value.scrollWidth - cardSlider.value.clientWidth >= nowScrollX.value + articleWidth.value + 16) {
+                nowScrollX.value += (articleWidth.value + 16);
+            }
+
+            cardSlider.value.scrollLeft = nowScrollX.value;
+        };
+
+        return {
+            type,
+            articleWidth,
+            nowScrollX,
+            cardSlider,
+            clickPrev,
+            clickNext
+        };
+    }
+  };
+  </script>
+
+<style scoped>
+.no-scrollbar::-webkit-scrollbar {
+  display: none; /* Chrome, Safari, Edge, and Opera */
+}
+
+.no-scrollbar {
+  -ms-overflow-style: none; /* Internet Explorer 10+ */
+  scrollbar-width: none; /* Firefox */
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -110,102 +110,9 @@
           @update:selectedOption="handleSelection"
         />
       </div>
-      <div
-        class="flex gap-4 justify-between mt-6 text-base font-medium text-neutral-700 max-md:flex-wrap"
-      >
-        <div
-          class="justify-center flex items-center px-5 my-auto bg-black rounded-full h-[55px] w-[55px] max-md:pr-5"
-        >
-          <img
-            src="./../assets/img/left_arrow.png"
-            alt="left_arrow1"
-            class="shrink-0 max-w-full"
-          />
-        </div>
-
-        <div
-          class="flex flex-1 gap-4 mt-6 text-base font-medium text-neutral-700 max-md:flex-wrap overflow-scroll no-scrollbar"
-        >
-          <article
-            class="flex flex-col relative pt-10 px-6 bg-white rounded-xl border-2 border-blue-500 border-solid pb-[124px]"
-          >
-            <div class="self-center text-xl font-bold">마음알기 설문1</div>
-            <div class="mt-8 max-md:mr-2.5">2023. 09. 01 ~ 12. 01</div>
-            <div class="self-center mt-3.5 text-red-600">D-70</div>
-            <div class="w-[206px] flex gap-1.5">
-              <button
-                class="items-center flex-1 px-2 py-2.5 mt-8 text-white whitespace-nowrap bg-blue-500 rounded-[30px] max-md:px-5 max-md:mr-2.5 overflow-hidden text-ellipsis"
-                v-if="loginType === 'student'"
-                @click="() => goReportNotice(1)"
-              >
-                시작하기
-              </button>
-              <button
-                class="items-center flex-1 px-2 py-2.5 mt-8 text-white whitespace-nowrap bg-blue-500 rounded-[30px] max-md:px-5 max-md:mr-2.5 overflow-hidden text-ellipsis"
-                v-if="loginType === 'teacher'"
-                @click="() => copyURL(1)"
-              >
-                URL 복사
-              </button>
-              <button
-                class="items-center flex-1 px-2 py-2.5 mt-8 text-white whitespace-nowrap bg-blue-700 rounded-[30px] max-md:px-5 max-md:mr-2.5 overflow-hidden text-ellipsis"
-                v-if="loginType === 'teacher'"
-                @click="() => openQRCodePopup(1)"
-              >
-                QR 코드
-              </button>
-            </div>
-            <img
-              src="./../assets/img/person_icn.png"
-              alt="person_icn"
-              class="self-end max-w-full aspect-[1.28] w-[116px] absolute bottom-0 right-1.5"
-            />
-          </article>
-          <article
-            class="flex flex-col relative pt-10 px-6 bg-white rounded-xl border-2 border-blue-500 border-solid pb-[124px]"
-          >
-            <div class="self-center text-xl font-bold">마음알기 설문2</div>
-            <div class="mt-8 max-md:mr-2.5">2023. 09. 01 ~ 12. 01</div>
-            <div class="self-center mt-3.5 text-red-600">D-70</div>
-            <div class="w-[206px] flex gap-1.5">
-              <button
-                class="items-center flex-1 px-2 py-2.5 mt-8 text-white whitespace-nowrap bg-blue-700 rounded-[30px] max-md:px-5 max-md:mr-2.5 overflow-hidden text-ellipsis"
-                v-if="loginType === 'student'"
-              >
-                이어서 진행하기
-              </button>
-              <button
-                class="items-center flex-1 px-2 py-2.5 mt-8 text-white whitespace-nowrap bg-blue-500 rounded-[30px] max-md:px-5 max-md:mr-2.5 overflow-hidden text-ellipsis"
-                v-if="loginType === 'teacher'"
-              >
-                URL 복사
-              </button>
-              <button
-                class="items-center flex-1 px-2 py-2.5 mt-8 text-white whitespace-nowrap bg-blue-700 rounded-[30px] max-md:px-5 max-md:mr-2.5 overflow-hidden text-ellipsis"
-                v-if="loginType === 'teacher'"
-                @click="openQRCodePopup"
-              >
-                QR 코드
-              </button>
-            </div>
-            <img
-              src="./../assets/img/person_icn.png"
-              alt="person_icn"
-              class="self-end max-w-full aspect-[1.28] w-[116px] absolute bottom-0 right-1.5"
-            />
-          </article>
-        </div>
-
-        <div
-          class="justify-center flex items-center px-5 my-auto bg-black rounded-full h-[55px] w-[55px] max-md:pr-5"
-        >
-          <img
-            src="./../assets/img/right_arrow.png"
-            alt="right_arrow1"
-            class="shrink-0 max-w-full"
-          />
-        </div>
-      </div>
+      <AppCardItem 
+        :options="1"
+      />
 
       <!-- 목록2 -->
       <div
@@ -218,58 +125,9 @@
           </div>
         </div>
       </div>
-      <div
-        class="flex gap-4 justify-between mt-6 text-base font-medium text-neutral-700 max-md:flex-wrap"
-      >
-        <div
-          class="justify-center flex items-center px-5 my-auto bg-black rounded-full h-[55px] w-[55px] max-md:pr-5"
-        >
-          <img
-            src="./../assets/img/left_arrow.png"
-            alt="left_arrow2"
-            class="shrink-0 max-w-full"
-          />
-        </div>
-
-        <div
-          class="flex flex-1 gap-4 mt-6 text-base font-medium text-neutral-700 max-md:flex-wrap overflow-scroll no-scrollbar"
-        >
-          <article
-            class="flex flex-col relative pt-10 px-6 bg-white rounded-xl border-2 border-zinc-600 border-solid pb-[124px]"
-          >
-            <div
-              class="self-center text-xl font-bold text-center text-neutral-700"
-            >
-              마음알기 설문5
-            </div>
-            <div
-              class="mt-8 text-base font-medium text-neutral-700 max-md:mr-2.5"
-            >
-              2023. 09. 01 ~ 12. 01
-            </div>
-            <button
-              class="justify-center w-[206px] items-center px-5 py-3 mt-8 text-white whitespace-nowrap bg-zinc-600 rounded-[30px] max-md:px-5 max-md:mr-2.5 overflow-hidden"
-            >
-              활동 참여가 완료되었어요!
-            </button>
-            <img
-              src="./../assets/img/person_bubble_icn.png"
-              alt="person_icn"
-              class="self-end max-w-full aspect-[1.6] w-[170px] absolute bottom-0 right-1.5"
-            />
-          </article>
-        </div>
-
-        <div
-          class="justify-center flex items-center px-5 my-auto bg-black rounded-full h-[55px] w-[55px] max-md:pr-5"
-        >
-          <img
-            src="./../assets/img/right_arrow.png"
-            alt="right_arrow2"
-            class="shrink-0 max-w-full"
-          />
-        </div>
-      </div>
+      <AppCardItem 
+        :options="2"
+      />
     </div>
   </div>
 </template>
@@ -280,6 +138,7 @@ import { useUserStore } from '@/store/userStore.js';
 import AppDropdown from '../components/AppDropdown.vue';
 import AppTopAlert from '../components/AppTopAlert.vue';
 import AppQRPopup from '../components/AppQRPopup.vue';
+import AppCardItem from '../components/AppCardItem';
 
 export default defineComponent({
   name: 'HomeView',
@@ -287,6 +146,7 @@ export default defineComponent({
     AppDropdown,
     AppTopAlert,
     AppQRPopup,
+    AppCardItem
   },
   setup() {
     const router = useRouter();
@@ -297,12 +157,16 @@ export default defineComponent({
     const showAlert = ref(false);
     const textAlert = ref('');
     const qrURL = ref('');
+    const cardSlider = ref(null);
+    const articleWidth = ref(0);
+    const nowScrollX = ref(0);
 
     const handleSelection = (option) => {
       selectedOption.value = option;
     };
 
-    onMounted(async () => {});
+    onMounted(async () => {
+    });
 
     const goReportNotice = (type = 1) => {
       router.push({
@@ -327,7 +191,7 @@ export default defineComponent({
       }
     };
 
-    const openQRCodePopup = (type = 1) => {
+    const openQRCodePopup = (type = 1) => { 
       const url = `${window.location.host}/report/notice/${type}`;
       qrURL.value = `https://quickchart.io/qr?text=${url}&size=250`;
     };
@@ -343,12 +207,13 @@ export default defineComponent({
       showAlert,
       textAlert,
       qrURL,
+      
 
       handleSelection,
       goReportNotice,
       copyURL,
       openQRCodePopup,
-      closePopup,
+      closePopup
     };
   },
 });
@@ -363,4 +228,5 @@ export default defineComponent({
   -ms-overflow-style: none; /* Internet Explorer 10+ */
   scrollbar-width: none; /* Firefox */
 }
+
 </style>
