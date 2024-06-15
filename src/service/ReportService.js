a12a3dfcad832f26bfe6eecb2fad2f223e6fcedd
hwansoo <realyhs1@gmail.com>
2024-06-15 21:11:07 +0900

    config 수정, service 예제 수정

--- src/service/ReportService.js
+++ src/service/ReportService.js
@@ -3,7 +3,7 @@ import apiClient from './index';
 /**
  * 예제
  * export const getAPI = (data = {}) => {
- * return apiClient.get('asdf', params: data)
+ * return apiClient.get('asdf', {params: data})
  * }
  * export const postAPI = (data = {}) => {
  * return apiClient.post('asdf', data)
