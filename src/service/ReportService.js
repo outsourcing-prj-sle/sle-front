4a635e73bbc9c9eac5ef5b51d3a2386c900b9797
hwansoo <realyhs1@gmail.com>
2024-06-16 13:14:59 +0900

    리포트 서비스 예제 수정

--- src/service/ReportService.js
+++ src/service/ReportService.js
@@ -8,4 +8,23 @@ import apiClient from './index';
  * export const postAPI = (data = {}) => {
  * return apiClient.post('asdf', data)
  * }
+ *
+ * 프론트 사용 예시
+ * const signupResponse = await UserService.checkId({
+ * id: username,
+ * });
+ * const resData = signupResponse.data;
+ *
+ * if (resData.error) {
+ * alert(resData.error);
+ * return;
+ * }
  */
+
+const login = (data = {}) => {
+  return apiClient.get('/users', data);
+};
+
+export default {
+  login,
+};
