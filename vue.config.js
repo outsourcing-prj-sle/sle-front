79467608a2c98b28a7976aaba277c48883ea44c8
hwansoo <realyhs1@gmail.com>
2024-06-17 06:33:12 +0900

    api url 파싱

--- vue.config.js
+++ vue.config.js
@@ -38,7 +38,7 @@ module.exports = defineConfig({
       '/api': {
         target: 'http://1.242.49.61:12000',
         changeOrigin: true,
-        pathRewrite: { '^/api': '' },
+        // pathRewrite: { '^/api': '' },
       },
     },
   },
