27eba90d93a7c9f0cfe79e6a779ffb161d8bd89f
hwansoo <realyhs1@gmail.com>
2024-06-29 22:55:53 +0900

    apiclient 재정의

--- src/service/AdminService.js
+++ src/service/AdminService.js
@@ -1,5 +1,69 @@
-import apiClient from './index';
+import axios from 'axios';
+import { useAdminStore } from '@/store/adminStore.js';
+import { getActivePinia } from 'pinia';
+
 const baseURL = '/admin';
+
+// Pinia가 활성화될 때까지 기다리기
+function getAdminStore() {
+  if (!getActivePinia()) {
+    throw new Error('Pinia has not been initialized');
+  }
+  return useAdminStore();
+}
+
+const apiClient = axios.create({
+  baseURL:
+    process.env.VUE_APP_PRODUCTION === 'live'
+      ? 'http://1.213.164.252:60080/api'
+      : 'http://localhost:5173/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+apiClient.interceptors.request.use(
+  (config) => {
+    let adminStore;
+
+    try {
+      adminStore = getAdminStore();
+    } catch (error) {
+      console.error(error.message);
+    }
+
+    // 관리자 페이지 헤더 설정
+    if (adminStore) {
+      config.headers.Authorization = adminStore.token || 'USRCNFRM_00000000004';
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+apiClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response) {
+      // 에러 응답이 존재할 경우
+      const status = error.response.status;
+      const errorMessage = error.response.statusText || 'An error occurred';
+
+      // 404 에러인 경우
+      if (status === 404) {
+        return { data: { status: 404, error: 'Not Found' } };
+      }
+
+      // 그 외의 에러인 경우
+      return { data: { status, error: errorMessage } };
+    }
+    // 오류 응답을 처리
+    return Promise.reject(error);
+  }
+);
 
 /**
  * 예제
--- src/service/ReportService.js
+++ src/service/ReportService.js
@@ -1,5 +1,69 @@
-import apiClient from './index';
+import axios from 'axios';
+import { useUserStore } from '@/store/userStore.js';
+import { getActivePinia } from 'pinia';
+
 const baseURL = '/reports';
+
+// Pinia가 활성화될 때까지 기다리기
+function getUserStore() {
+  if (!getActivePinia()) {
+    throw new Error('Pinia has not been initialized');
+  }
+  return useUserStore();
+}
+
+const apiClient = axios.create({
+  baseURL:
+    process.env.VUE_APP_PRODUCTION === 'live'
+      ? 'http://1.213.164.252:60080/api'
+      : 'http://localhost:5173/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+apiClient.interceptors.request.use(
+  (config) => {
+    let userStore;
+
+    try {
+      userStore = getUserStore();
+    } catch (error) {
+      console.error(error.message);
+    }
+
+    // 사용자 페이지 헤더 설정
+    if (userStore) {
+      config.headers.Authorization = userStore.token || 'USRCNFRM_00000000004';
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+apiClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response) {
+      // 에러 응답이 존재할 경우
+      const status = error.response.status;
+      const errorMessage = error.response.statusText || 'An error occurred';
+
+      // 404 에러인 경우
+      if (status === 404) {
+        return { data: { status: 404, error: 'Not Found' } };
+      }
+
+      // 그 외의 에러인 경우
+      return { data: { status, error: errorMessage } };
+    }
+    // 오류 응답을 처리
+    return Promise.reject(error);
+  }
+);
 
 /**
  * 예제
