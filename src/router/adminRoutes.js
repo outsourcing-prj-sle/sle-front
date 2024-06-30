d7262053dd0f44e9462f10b5069712663e6bf097
hwansoo <realyhs1@gmail.com>
2024-06-30 12:23:53 +0900

    관리자 라우팅

--- src/router/adminRoutes.js
+++ src/router/adminRoutes.js
@@ -1,3 +1,25 @@
-const adminRoutes = [];
+import AdminLayout from '@/layouts/AdminLayout.vue';
+import adminLoginView from '@/views/admin/adminLoginView.vue';
+
+const adminRoutes = [
+  {
+    path: '/admin',
+    component: AdminLayout,
+    children: [
+      // {
+      //   path: '',
+      //   name: 'home',
+      //   component: HomeView,
+      //   meta: { footerVisible: true, headerVisible: true, needLogin: true },
+      // },
+      {
+        path: 'login',
+        name: 'adminLogin',
+        component: adminLoginView,
+        meta: { footerVisible: false, headerVisible: true },
+      },
+    ],
+  },
+];
 
 export default adminRoutes;
