0b514484ff4f927b3c19d1c8087e479561835e21
hwansoo <realyhs1@gmail.com>
2024-07-01 21:01:39 +0900

    관리자 로그인

--- src/views/admin/adminLoginView.vue
+++ src/views/admin/adminLoginView.vue
@@ -4,16 +4,41 @@
       class="w-full max-w-md content-center p-8 space-y-8 min-h-[350px] bg-white rounded-[20px] shadow-[3px_6px_14px_6px_rgba(0,0,0,0.1)] absolute top-1/2 left-1/2"
       style="transform: translate(-50%, -50%)"
     >
-      <div class="flex justify-center mb-[60px]">
+      <div class="flex justify-center mb-4">
         <img src="@/assets/img/logo_sel.png" alt="Logo" class="h-24" />
       </div>
       <div class="text-2xl font-semibold">관리자 로그인</div>
-
-      <div class="flex items-center justify-center">
+      <div class="space-y-4">
+        <div>
+          <label for="email" class="block text-sm font-medium text-gray-700"
+            >아이디</label
+          >
+          <input
+            type="text"
+            id="email"
+            v-model="email"
+            required
+            class="w-full px-3 py-2 mt-1 text-sm border rounded focus:outline-none focus:ring focus:ring-blue-200"
+          />
+        </div>
+        <div>
+          <label for="password" class="block text-sm font-medium text-gray-700"
+            >비밀번호</label
+          >
+          <input
+            type="password"
+            id="password"
+            v-model="password"
+            required
+            class="w-full px-3 py-2 mt-1 text-sm border rounded focus:outline-none focus:ring focus:ring-blue-200"
+          />
+        </div>
+      </div>
+      <div class="flex items-center justify-between mt-6">
         <button
           @click="submitLogin"
           type="submit"
-          class="w-[230px] text-center items-center flex justify-center px-4 py-2 text-base font-medium text-white bg-gray-600 rounded hover:opacity-80 focus:outline-none"
+          class="w-full px-4 py-2 text-sm font-medium text-white bg-gray-600 rounded hover:opacity-80 focus:outline-none"
         >
           관리자 로그인
         </button>
@@ -25,7 +50,7 @@
 <script>
 import { ref, onBeforeMount } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
-import { userAdminStore } from '@/store/adminStore.js';
+import { useAdminStore } from '@/store/adminStore.js';
 import AdminService from '@/service/AdminService.js';
 
 export default {
@@ -34,7 +59,7 @@ export default {
   setup() {
     const route = useRoute();
     const router = useRouter();
-    const adminStore = userAdminStore();
+    const adminStore = useAdminStore();
     const email = ref('');
     const password = ref('');
 
@@ -71,7 +96,7 @@ export default {
         id: 'id',
       });
 
-      const redirectPath = route.query.redirect || '/admin';
+      const redirectPath = route.query.redirect || '/admin/site';
       router.push(redirectPath);
     };
     return {
