50091a149d343d004b8fe8d07e2f5cb25fa6b8ed
chw970320 <chw970320@gmail.com>
2024-07-04 02:24:43 +0900

    Design: ID톡톡 관리자 퍼블 프로토타입 완성

---
+++ src/components/id/IDTableCol.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="w-full overflow-auto">
+    <table
+        class="w-full border-t-2 border-gray-300"
+        v-if="data.length"
+    >
+      <tr v-for="item in data" :key="item">
+        <th class="bg-gray-100 px-4 text-left min-w-[100px]" :class="{ 'essential' : item.isRequired}">
+          {{ item.header }}
+        </th>
+        <td class="flex justify-start px-4 py-2">
+          <input v-if="item.body.isText" class="text-sm border border-gray-300 px-4 py-2 min-w-[300px] rounded-md" type="text" :placeholder="item.body.placeholder">
+          <div v-if="item.body.isPhone" class="flex gap-[10px] items-center">
+            <AppDropdown
+              v-if="selectedOption"
+              :options="options"
+              :startText="selectedOption"
+              @update:selectedOption="handleSelection1"
+            />
+            <input class="text-sm border border-gray-300 px-4 py-2 min-w-[150px]] rounded-md" type="text">
+            <input class="text-sm border border-gray-300 px-4 py-2 min-w-[150px] rounded-md" type="text">
+          </div>
+          <div v-if="item.body.isEmail" class="flex gap-[10px] items-center">
+            <input class="text-sm border border-gray-300 px-4 py-2 min-w-[150px]] rounded-md" type="text">
+            <p>@</p>
+            <input class="text-sm border border-gray-300 px-4 py-2 min-w-[150px] rounded-md" type="text">
+            <AppDropdown
+              v-if="selectedOption2"
+              :options="options2"
+              :startText="selectedOption2"
+              @update:selectedOption="handleSelection1"
+            />
+          </div>
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import { ref, computed } from 'vue';
+import AppDropdown from '@/components/AppDropdown.vue';
+
+export default {
+  components: {
+    AppDropdown
+  },
+  props: {
+    data: {
+      type: Array,
+      default: () => [],
+    },
+    isRequired: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props, { emit }) {
+    const options = ref(['국번']);
+    const options2 = ref(['작접입력']);
+    const selectedOption = ref('국번');
+    const selectedOption2 = ref('직접입력');
+
+    return {
+      options,
+      options2,
+      selectedOption,
+      selectedOption2,
+    };
+  },
+};
+</script>
+
+<style>
+  th.essential::after {
+    content: '*';
+    color: red;
+    position: absolute;
+  }
+</style>
--- src/views/id/IDManageLtView.vue
+++ src/views/id/IDManageLtView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-start mx-[40px] my-[20px] max-md:mx-[20px] max-md:my-[20px]">
     <div class="flex flex-col gap-[20px] items-start w-full">
-      <p class="font-bold text-[20px] max-md:text-[16px]">설문관리 > {{ pollNm }}</p>
+      <p class="font-bold text-[20px] max-md:text-[16px]">학습성향</p>
       <div class="flex justify-between items-center w-full">
         <div class="flex items-center gap-[20px]">
           <AppDropdown
@@ -17,11 +17,6 @@
             @update:selectedOption="handleSelection1"
           />
         </div>
-        <IDSearch
-        :placeholder="'학교명을 입력해주세요.'"
-        />
-      </div>
-      <div class="flex justify-end w-full">
         <div class="text-blue-500 cursor-pointer" @click="downloadExcel">
           <img
             class="h-[38px]"
@@ -39,11 +34,6 @@
       :recordCount=10
       :totalCount=12
       />
-      <IDButton class="bg-gray-300"
-      :text="'목록으로'"
-      :isGray="true"
-      :_width="160"
-      />
     </div>
   </div>
 </template>
@@ -56,24 +46,19 @@ import IDService from '@/service/IDService.js';
 import IDTable from '@/components/id/IDTable.vue'
 import AppDropdown from '@/components/AppDropdown.vue'
 import IDPagination from '@/components/id/IDPagination.vue';
-import IDSearch from '@/components/id/IDSearch.vue';
-import IDButton from '@/components/id/IDButton.vue';
 
 export default {
-  name: 'IDManageReportDtlView',
+  name: 'IDManageLtView',
   components: {
     IDTable,
     AppDropdown,
     IDPagination,
-    IDSearch,
-    IDButton
 },
   setup() {
     const options = ref([123, 456]);
-    const selectedOption = ref('학교급');
+    const selectedOption = ref('학교');
     const options2 = ref([123, 456]);
-    const selectedOption2 = ref('학년');
-    const pollNm = ref("마음알기 설문1 상세");
+    const selectedOption2 = ref('정렬');
     const header = [
       {
         isCheckbox: true
@@ -82,40 +67,22 @@ export default {
         text: '번호'
       }, 
       {
-        text: '학교'
+        text: '학교명'
       }, 
       {
-        text: '학년'
+        text: '교사명'
       }, 
       {
-        text: '이름'
+        text: '담당 학년-반'
       }, 
       {
-        text: '성별'
+        text: 'Q1_1'
       }, 
       {
-        text: '생년-월'
+        text: 'Q1_2'
       },
       {
-        text: '계정'
-      },
-      {
-        text: 'EK1.1'
-      },
-      {
-        text: 'EK1.2'
-      },
-      {
-        text: 'EK1.3'
-      },
-      {
-        text: 'EK1.4'
-      },
-      {
-        text: 'EK1.5'
-      },
-      {
-        text: 'EK1.6'
+        text: 'Q1_3'
       },
     ];
     const body = [
@@ -123,17 +90,11 @@ export default {
         { isCheckbox: true },
         { text: 1 },
         { text: '이작초등학교' },
-        { text: '3' },
-        { text: '홍길동' },
-        { text: '남' },
-        { text: '2014-09' },
-        { text: 'test@gne.go.kr' },
-        { text: 3 },
-        { text: 1 },
-        { text: 1 },
-        { text: 2 },
-        { text: 4 },
-        { text: 1 }
+        { text: '김톡톡' },
+        { text: '3-01' },
+        { text: '1' },
+        { text: '2, 3' },
+        { text: '더 다양한 종류의 분석을 통하여 다양한 자료를 함께 볼 수 있었으면 좋겠습니다.' },
       ]
     ];
 
@@ -185,7 +146,6 @@ export default {
       selectedOption,
       options2,
       selectedOption2,
-      pollNm,
       header,
       body,
 
