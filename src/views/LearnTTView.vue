1465091b06b00c59a9e01b925f13405bbd6519e8
hwansoo <realyhs1@gmail.com>
2024-07-07 04:23:34 +0900

    라인차트

--- src/views/LearnTTView.vue
+++ src/views/LearnTTView.vue
@@ -352,17 +352,11 @@ export default {
     const barChartSeries = ref([
       {
         name: 'Males',
-        data: [
-          0.4, 0.65, 0.76, 0.88, 1.5, 2.1, 2.9, 3.8, 3.9, 4.2, 4, 4.3, 4.1, 4.2,
-          4.5, 3.9, 3.5, 3,
-        ],
+        data: [0.4, 0.65, 0.76, 0.88, 1.5, 2.1],
       },
       {
         name: 'Females',
-        data: [
-          -0.8, -1.05, -1.06, -1.18, -1.4, -2.2, -2.85, -3.7, -3.96, -4.22,
-          -4.3, -4.4, -4.1, -4, -4.1, -3.4, -3.1, -2.8,
-        ],
+        data: [-0.8, -1.05, -1.06, -1.18, -1.4, -2.2],
       },
     ]);
 
@@ -372,9 +366,17 @@ export default {
         height: 440,
         stacked: true,
       },
-      colors: ['#008FFB', '#FF4560'],
+      colors: [
+        '#33b2df',
+        '#546E7A',
+        '#d4526e',
+        '#13d8aa',
+        '#A5978B',
+        '#2b908f',
+      ],
       plotOptions: {
         bar: {
+          distributed: true, // << 이 옵션 꼭 필요
           borderRadius: 5,
           borderRadiusApplication: 'end', // 'around', 'end'
           borderRadiusWhenStacked: 'all', // 'all', 'last'
@@ -402,49 +404,20 @@ export default {
       },
       tooltip: {
         shared: false,
-        x: {
-          formatter: function (val) {
-            return val;
-          },
-        },
-        y: {
-          formatter: function (val) {
-            return Math.abs(val) + '%';
-          },
-        },
       },
       title: {
         text: 'Mauritius population pyramid 2011',
       },
       xaxis: {
-        categories: [
-          '85+',
-          '80-84',
-          '75-79',
-          '70-74',
-          '65-69',
-          '60-64',
-          '55-59',
-          '50-54',
-          '45-49',
-          '40-44',
-          '35-39',
-          '30-34',
-          '25-29',
-          '20-24',
-          '15-19',
-          '10-14',
-          '5-9',
-          '0-4',
-        ],
+        // categories: ['15-19', '10-14', '5-9', '0-4'],
         title: {
           text: 'Percent',
         },
-        labels: {
-          formatter: function (val) {
-            return Math.abs(Math.round(val)) + '%';
-          },
-        },
+        // labels: {
+        //   formatter: function (val) {
+        //     return Math.abs(Math.round(val)) + '%';
+        //   },
+        // },
       },
     });
 
