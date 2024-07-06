db28293a71fbd77bbd57e4df52256f10c2b1f98b
hwansoo <realyhs1@gmail.com>
2024-07-06 19:08:50 +0900

    사이트 관리 등록

--- src/components/admin/AdminTableCol.vue
+++ src/components/admin/AdminTableCol.vue
@@ -1,38 +1,119 @@
 <template>
   <div class="w-full overflow-y-visible">
-    <table
-        class="w-full border-t-2 border-gray-300"
-        v-if="data.length"
-    >
-      <tr v-for="item in data" :key="item">
-        <th class="bg-gray-100 px-4 text-left min-w-[100px] text-sm text-nowrap" :class="{ 'essential' : item.isRequired}">
+    <table class="w-full border-t-2 border-gray-300" v-if="data && data.length">
+      <tr v-for="(item, index) in data" :key="item">
+        <th
+          class="bg-gray-100 px-4 text-left min-w-[100px] text-sm text-nowrap min-h-11"
+          :class="{ essential: item.isRequired }"
+        >
           {{ item.header }}
         </th>
-        <td class="flex justify-start px-4 py-2">
-          <input v-if="item.body.isText" class="text-sm border border-gray-300 px-4 py-2 min-w-[300px] rounded-md" type="text" :placeholder="item.body.placeholder">
-          <div v-if="item.body.isPhone" class="flex gap-[10px] items-center">
+        <td class="flex justify-start px-4 py-2 min-h-11">
+          <div v-if="item.body.isCheckText">
+            <div class="flex gap-2.5">
+              <input
+                class="text-sm border border-gray-300 px-4 py-2 min-w-[300px] rounded-md"
+                type="text"
+                :placeholder="item.body.placeholder"
+                :value="item.value"
+                @input="updateValue($event.target.value, index)"
+              />
+              <AdminButton
+                :text="'중복확인'"
+                :isWhite="true"
+                @onClick="() => updateEvent('', index, 'checkSite')"
+              />
+            </div>
+            <div
+              v-if="item.isChecked"
+              class="text-xs text-green-400 mt-1 text-left"
+            >
+              사용가능한 도메인입니다.
+            </div>
+          </div>
+          <div
+            v-else-if="item.body.isPhone"
+            class="flex gap-[10px] items-center"
+          >
             <AppDropdown
               v-if="selectedOption"
               :options="options"
               :startText="selectedOption"
               :openFull="true"
-              @update:selectedOption="handleSelection1"
+              @update:selectedOption="(v) => handleSelection('isPhone', v)"
             />
-            <input class="text-sm border border-gray-300 px-4 py-2 min-w-[150px]] rounded-md" type="text">
-            <input class="text-sm border border-gray-300 px-4 py-2 min-w-[150px] rounded-md" type="text">
+            <input
+              class="text-sm border border-gray-300 px-4 py-2 min-w-[150px]] rounded-md"
+              type="text"
+            />
+            <input
+              class="text-sm border border-gray-300 px-4 py-2 min-w-[150px] rounded-md"
+              type="text"
+            />
           </div>
-          <div v-if="item.body.isEmail" class="flex gap-[10px] items-center">
-            <input class="text-sm border border-gray-300 px-4 py-2 min-w-[150px]] rounded-md" type="text">
+          <div
+            v-else-if="item.body.isEmail"
+            class="flex gap-[10px] items-center"
+          >
+            <input
+              class="text-sm border border-gray-300 px-4 py-2 min-w-[150px]] rounded-md"
+              type="text"
+            />
             <p>@</p>
-            <input class="text-sm border border-gray-300 px-4 py-2 min-w-[150px] rounded-md" type="text">
+            <input
+              class="text-sm border border-gray-300 px-4 py-2 min-w-[150px] rounded-md"
+              type="text"
+            />
             <AppDropdown
               v-if="selectedOption2"
               :options="options2"
               :startText="selectedOption2"
               :openFull="true"
-              @update:selectedOption="handleSelection1"
+              @update:selectedOption="(v) => handleSelection('isEmail', v)"
             />
           </div>
+          <div class="flex gap-2.5" v-if="item.body.isYesNo">
+            <input
+              type="radio"
+              :id="`isYesNo_yes${index}`"
+              value="1"
+              v-model="item.value"
+              @input="updateValue(1, index)"
+            />
+            <label for="isYesNo_yes">예</label>
+
+            <input
+              type="radio"
+              class="ml-10"
+              :id="`isYesNo_no${index}`"
+              value="0"
+              v-model="item.value"
+              @input="updateValue(0, index)"
+            />
+            <label for="isYesNo_no">아니요</label>
+          </div>
+          <textarea
+            v-else-if="item.body.isTextArea"
+            class="text-sm border border-gray-300 px-4 py-2 min-w-[300px] h-[100px] resize-none overflow-y-scroll rounded-md"
+            :placeholder="item.body.placeholder"
+            :value="item.value"
+            @input="updateValue($event.target.value, index)"
+          >
+          </textarea>
+          <div v-if="item.body.isUpload">
+            <input
+              type="file"
+              @change="(e) => updateValue(e.target.files[0], index)"
+            />
+          </div>
+          <input
+            v-else-if="item.body.isText"
+            class="text-sm border border-gray-300 px-4 py-2 min-w-[300px] rounded-md"
+            type="text"
+            :placeholder="item.body.placeholder"
+            :value="item.value"
+            @input="updateValue($event.target.value, index)"
+          />
         </td>
       </tr>
     </table>
@@ -40,17 +121,29 @@
 </template>
 
 <script>
-import { ref, computed } from 'vue';
+import { ref, watch, onMounted } from 'vue';
 import AppDropdown from '@/components/AppDropdown.vue';
+import AdminButton from '@/components/admin/AdminButton.vue';
 
 export default {
   components: {
-    AppDropdown
+    AppDropdown,
+    AdminButton,
   },
   props: {
-    data: {
+    _data: {
       type: Array,
       default: () => [],
+    },
+    changeTableState: {
+      type: Object,
+      default: () => {
+        return {
+          index: null,
+          key: null,
+          value: null,
+        };
+      },
     },
     isRequired: {
       type: Boolean,
@@ -58,25 +151,83 @@ export default {
     },
   },
   setup(props, { emit }) {
+    const data = ref([]);
     const options = ref(['국번']);
     const options2 = ref(['작접입력']);
     const selectedOption = ref('국번');
     const selectedOption2 = ref('직접입력');
+
+    onMounted(() => {
+      data.value = props._data;
+      console.log(data.value);
+    });
+
+    watch(
+      () => props._data,
+      (newVal) => {
+        if (!data.value.length) data.value = newVal;
+      }
+    );
+
+    watch(
+      () => props.changeTableState,
+      (newVal) => {
+        console.log('yhs :: newVal');
+        console.log(newVal);
+        if (newVal.index) {
+          const index = newVal.index;
+          const key = newVal.key;
+          const value = newVal.value;
+          data.value[index][key] = value;
+        }
+      }
+    );
+
+    const updateValue = (value, index) => {
+      /**
+       * handleInputUpdate (valie, index, evnet)
+       * 해당 index의 value 값 수정
+       */
+      emit('handleInputUpdate', value, index);
+      data.value[index].value = value;
+    };
+
+    const updateEvent = (value, index, event) => {
+      /**
+       * handleInputUpdate (valie, index)
+       * 해당 index의 event 값 수정
+       */
+      emit('handleEventUpdate', value, index, event);
+    };
+
+    const handleSelection = (e, v) => {
+      switch (e) {
+        case 'isPhone':
+          break;
+        case 'isEmail':
+          break;
+      }
+    };
 
     return {
       options,
       options2,
       selectedOption,
       selectedOption2,
+      data,
+
+      updateValue,
+      updateEvent,
+      handleSelection,
     };
   },
 };
 </script>
 
 <style>
-  th.essential::after {
-    content: '*';
-    color: red;
-    position: absolute;
-  }
+th.essential::after {
+  content: '*';
+  color: red;
+  position: absolute;
+}
 </style>
--- src/views/admin/adminSiteUpdateView.vue
+++ src/views/admin/adminSiteUpdateView.vue
@@ -4,14 +4,15 @@
   >
     <div class="flex flex-col gap-[20px] items-start w-full">
       <p class="font-bold text-[20px] max-md:text-[16px]">사이트관리 등록</p>
-      <AdminTableCol :data="data" :isRequired="true" />
+      <AdminTableCol
+        :_data="data"
+        :changeTableState="changeTableState"
+        @handleInputUpdate="handleInputUpdate"
+        @handleEventUpdate="handleEventUpdate"
+      />
       <div class="flex w-full gap-[20px] justify-center">
-        <AdminButton :text="'등록'" :isWhite="false" />
-        <AdminButton
-          :text="'목록'"
-          :isWhite="true"
-          @onClick="goResearchListVw"
-        />
+        <AdminButton :text="'등록'" :isWhite="false" @onClick="submit" />
+        <AdminButton :text="'목록'" :isWhite="true" @onClick="goSite" />
       </div>
     </div>
   </div>
@@ -31,66 +32,198 @@ export default {
     AdminTableCol,
     AdminButton,
   },
-  setup() {
+  props: {
+    isUpdate: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props) {
     const router = useRouter();
-    const data = [
-      {
-        header: '이름',
-        body: {
-          isText: true,
-        },
-        isRequired: true,
-      },
-      {
-        header: '아이디',
-        body: {
-          isText: true,
-        },
-        isRequired: true,
-      },
-      {
-        header: '비밀번호',
-        body: {
-          isText: true,
-        },
-        isRequired: true,
-      },
-      {
-        header: '회원구분',
-        body: {
-          isText: true,
-          placeholder: '연구소관리자',
-        },
-      },
-      {
-        header: '핸드폰번호',
-        body: {
-          isPhone: true,
-        },
-        isRequired: true,
-      },
-      {
-        header: '이메일',
-        body: {
-          isEmail: true,
-        },
-        isRequired: true,
-      },
-    ];
+    const data = ref([]);
+    const changeTableState = ref({
+      index: null,
+      key: null,
+      value: null,
+    });
+    const checkSiteText = ref('');
+    const siteId = ref();
+
+    onMounted(() => {
+      if (!props.isUpdate) {
+        data.value = [
+          {
+            header: '사이트 명',
+            body: {
+              isText: true,
+            },
+            value: '',
+            isRequired: true,
+          },
+          {
+            header: '사이트 도메인',
+            body: {
+              isCheckText: true,
+            },
+            value: '',
+            isRequired: true,
+          },
+          {
+            header: '상단로고',
+            body: {
+              isUpload: true,
+            },
+          },
+          {
+            header: '하단로고',
+            body: {
+              isUpload: true,
+            },
+          },
+          {
+            header: '대표이미지',
+            body: {
+              isUpload: true,
+            },
+          },
+          {
+            header: '사이트설명',
+            body: {
+              isTextArea: true,
+              placeholder: '설명을 입력하세요.',
+            },
+          },
+          {
+            header: '마우스 보안허용',
+            body: {
+              isYesNo: true,
+            },
+            isRequired: true,
+          },
+          {
+            header: '키보드 보안허용',
+            body: {
+              isYesNo: true,
+            },
+            isRequired: true,
+          },
+        ];
+      } else {
+        // data 받아오기
+        siteId.value = 'site_001';
+      }
+    });
 
     const handleSelection1 = (v) => {
       console.log(v);
     };
 
-    const goResearchListVw = () => {
-      router.push({ name: 'IDManageResearchView' });
+    const goSite = () => {
+      router.push({ name: 'site' });
+    };
+
+    const handleInputUpdate = (v, i) => {
+      if (i === 1 && checkSiteText.value)
+        setChangeTableState(1, 'isChecked', false);
+      if (data.value[i]) data.value[i].value = v;
+    };
+
+    const setChangeTableState = (i, e, v) => {
+      changeTableState.value = {
+        index: i,
+        key: e,
+        value: v,
+      };
+    };
+
+    const handleEventUpdate = (v, i, e) => {
+      switch (e) {
+        case 'checkSite':
+          checkSite(data.value[i].value);
+          break;
+        default:
+          data.value[i][e] = v;
+          break;
+      }
+    };
+
+    const checkSite = async (v) => {
+      if (!v) return;
+      const checkSiteResponse = await AdminService.checkSystemExist('site', {
+        url: v,
+      });
+      const resData = checkSiteResponse.data;
+
+      if (resData.error) {
+        alert(resData.error);
+        return;
+      }
+
+      if (resData) {
+        alert('중복 url이 있습니다.');
+      } else {
+        checkSiteText.value = v;
+        setChangeTableState(1, 'isChecked', true);
+      }
+    };
+
+    const submit = async () => {
+      let formData = new FormData();
+      if (siteId.value) formData.append('siteId', siteId.value);
+
+      if (data.value[0].value) formData.append('siteName', data.value[0].value);
+      else {
+        alert('사이트 명을 입력해주세요.');
+        return;
+      }
+      if (data.value[1].value)
+        formData.append('siteDomain', data.value[1].value);
+      else {
+        alert('사이트 도메인을 입력해주세요.');
+        return;
+      }
+      if (data.value[2].value)
+        formData.append('topLogoImage', data.value[2].value);
+      if (data.value[3].value)
+        formData.append('bottomLogoImage', data.value[3].value);
+      if (data.value[4].value)
+        formData.append('mainImage', data.value[4].value);
+      if (data.value[5].value)
+        formData.append('siteDescription', data.value[5].value);
+      if (data.value[6].value)
+        formData.append('mouseSecurity', parseInt(data.value[6].value));
+      else {
+        alert('마우스 보안허용을 선택해주세요.');
+        return;
+      }
+      if (data.value[7].value)
+        formData.append('keyboardSecurity', parseInt(data.value[7].value));
+      else {
+        alert('키보드 보안허용을 선택해주세요.');
+        return;
+      }
+
+      let submitResult;
+      if (props.isUpdate) {
+        submitResult = await AdminService.updateSystem('site', formData);
+      } else {
+        submitResult = await AdminService.insertSystem('site', formData);
+      }
+
+      const resData = submitResult.data;
+      console.log(resData);
+      // router.push({ name: 'adminSite' });
     };
 
     return {
       data,
 
       handleSelection1,
-      goResearchListVw,
+      goSite,
+      changeTableState,
+      handleInputUpdate,
+      handleEventUpdate,
+      submit,
     };
   },
 };
