b75ab1e9ccba8e0edbccf6701d2b33fd5f9203c4
hwansoo <realyhs1@gmail.com>
2024-07-07 17:03:23 +0900

    상세보기 목록 확인

--- src/views/id/IDManageReportDtlView.vue
+++ src/views/id/IDManageReportDtlView.vue
@@ -1,24 +1,31 @@
 <template>
-  <div class="flex justify-start mx-[40px] my-[20px] max-md:mx-[20px] max-md:my-[20px]">
+  <div
+    class="flex justify-start mx-[40px] my-[20px] max-md:mx-[20px] max-md:my-[20px]"
+  >
     <div class="flex flex-col gap-[20px] items-start w-full">
-      <p class="font-bold text-[20px] max-md:text-[16px]">설문관리 > {{ pollNm }}</p>
+      <p class="font-bold text-[20px] max-md:text-[16px]">
+        설문관리 > {{ pollNm }} 상세
+      </p>
       <div class="flex justify-between items-center w-full">
         <div class="flex items-center gap-[20px]">
           <AppDropdown
-            :options="options"
-            :startText="selectedOption"
+            :objectOptions="options1"
+            :startText="selectedOption1"
             :openFull="true"
             @update:selectedOption="handleSelection1"
           />
           <AppDropdown
-            :options="options2"
+            :objectOptions="options2"
             :startText="selectedOption2"
+            :updateText="updateText2"
             :openFull="true"
-            @update:selectedOption="handleSelection1"
+            @update:selectedOption="handleSelection2"
           />
         </div>
         <IDSearch
-        :placeholder="'학교명을 입력해주세요.'"
+          v-model="searchText"
+          :placeholder="'학교명을 입력해주세요.'"
+          @keyup.enter="searchSch"
         />
       </div>
       <div class="flex justify-end w-full">
@@ -30,36 +37,34 @@
           />
         </div>
       </div>
-      <IDTable
-      :header="header"
-      :_body="body"
-      />
+      <IDTable :header="header" :_body="body" />
       <IDPagination
-      :pageNo=pageNo
-      :recordCount=recordCount
-      :totalCount=totalCount
+        :pageNo="page"
+        :recordCount="10"
+        :totalCount="totalCount"
+        @updatePage="updatePage"
       />
-      <IDButton class="bg-gray-300"
-      @click="goReportList"
-      :text="'목록으로'"
-      :isGray="true"
-      :_width="160"
+      <IDButton
+        class="bg-gray-300"
+        @click="goReportList"
+        :text="'목록으로'"
+        :isGray="true"
+        :_width="160"
       />
     </div>
   </div>
 </template>
 
 <script>
-import { ref, onBeforeMount, computed } from 'vue';
+import { ref, onBeforeMount, computed, onMounted } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
 import { useIDStore } from '@/store/IDStore.js';
 import IDService from '@/service/IDService.js';
-import IDTable from '@/components/id/IDTable.vue'
-import AppDropdown from '@/components/AppDropdown.vue'
+import IDTable from '@/components/id/IDTable.vue';
+import AppDropdown from '@/components/AppDropdown.vue';
 import IDPagination from '@/components/id/IDPagination.vue';
 import IDSearch from '@/components/id/IDSearch.vue';
 import IDButton from '@/components/id/IDButton.vue';
-import { onMounted } from 'vue';
 
 export default {
   name: 'IDManageReportDtlView',
@@ -68,48 +73,102 @@ export default {
     AppDropdown,
     IDPagination,
     IDSearch,
-    IDButton
-},
+    IDButton,
+  },
   setup() {
+    const route = useRoute();
     const router = useRouter();
-    const options = ref([123, 456]);
-    const selectedOption = ref('학교급');
-    const options2 = ref([123, 456]);
+    const pollId = ref(route.query.id);
+    const searchText = ref();
+    const searchTextComplete = ref();
+    const options1 = ref([
+      {
+        name: '학교급',
+        value: '',
+      },
+      {
+        name: '초등학교',
+        value: 'SCH_02',
+      },
+      {
+        name: '중학교',
+        value: 'SCH_03',
+      },
+      {
+        name: '고등학교',
+        value: 'SCH_04',
+      },
+    ]);
+
+    const selectedOption1 = ref('학교급');
+    const valueOption1 = ref('');
+    const options2 = ref([
+      {
+        name: '학년',
+        value: '',
+      },
+      {
+        name: '1학년',
+        value: 'GRADE01',
+      },
+      {
+        name: '2학년',
+        value: 'GRADE02',
+      },
+      {
+        name: '3학년',
+        value: 'GRADE03',
+      },
+      {
+        name: '4학년',
+        value: 'GRADE04',
+      },
+      {
+        name: '5학년',
+        value: 'GRADE05',
+      },
+      {
+        name: '6학년',
+        value: 'GRADE06',
+      },
+    ]);
     const selectedOption2 = ref('학년');
-    const pollNm = ref("마음알기 설문1 상세");
+    const valueOption2 = ref('');
+    const updateText2 = ref();
+    const pollNm = ref('마음알기 설문1');
     const header = computed(() => {
       return [
         {
-          isCheckbox: true
+          isCheckbox: true,
         },
         {
-          text: '번호'
-        }, 
-        {
-          text: '학교'
-        }, 
-        {
-          text: '학년'
-        }, 
-        {
-          text: '이름'
-        }, 
-        {
-          text: '성별'
-        }, 
-        {
-          text: '생년-월'
+          text: '번호',
         },
         {
-          text: '계정'
+          text: '학교',
         },
-        ...headerArr.value
+        {
+          text: '학년',
+        },
+        {
+          text: '이름',
+        },
+        {
+          text: '성별',
+        },
+        {
+          text: '생년-월',
+        },
+        {
+          text: '계정',
+        },
+        ...headerArr.value,
       ];
     });
     const headerArr = ref([]);
     const body = ref([]);
 
-    const pageNo = ref(1);
+    const page = ref(1);
     const recordCount = ref(10);
     const totalCount = ref(0);
 
@@ -118,22 +177,81 @@ export default {
     });
 
     const handleSelection1 = (v) => {
-      console.log(v);
+      valueOption1.value = v;
+      if (v === '' || v === 'SCH_02') {
+        options2.value = [
+          {
+            name: '학년',
+            value: '',
+          },
+          {
+            name: '1학년',
+            value: 'GRADE01',
+          },
+          {
+            name: '2학년',
+            value: 'GRADE02',
+          },
+          {
+            name: '3학년',
+            value: 'GRADE03',
+          },
+          {
+            name: '4학년',
+            value: 'GRADE04',
+          },
+          {
+            name: '5학년',
+            value: 'GRADE05',
+          },
+          {
+            name: '6학년',
+            value: 'GRADE06',
+          },
+        ];
+      } else {
+        options2.value = [
+          {
+            name: '학년',
+            value: '',
+          },
+          {
+            name: '1학년',
+            value: 'GRADE01',
+          },
+          {
+            name: '2학년',
+            value: 'GRADE02',
+          },
+          {
+            name: '3학년',
+            value: 'GRADE03',
+          },
+        ];
+      }
+      updateText2.value = '0';
+      setTimeout(() => {
+        updateText2.value = null;
+      }, 250);
+      fetchReportDtlList();
+    };
+
+    const handleSelection2 = (v) => {
+      valueOption2.value = v;
+      fetchReportDtlList();
     };
 
     const fetchReportDtlList = async () => {
-      const reportReponse = await IDService.getReportsDtl('QES00000000000000001', {
-        searchKeyword : '경남시범초',
-        schulGradeCode : '',
-        stGrade : '5',
-        pageNo : pageNo.value,
-        recordCount : recordCount.value,
+      const reportReponse = await IDService.getReportsDtl(pollId.value, {
+        searchKeyword: searchTextComplete.value,
+        schulGradeCode: valueOption1.value,
+        stGrade: valueOption1.value,
+        pageNo: page.value,
+        recordCount: recordCount.value,
       });
       const resData = reportReponse.data;
       console.log(resData);
 
-      pageNo.value = resData.pageNo;
-      recordCount.value = resData.recordCount;
       totalCount.value = resData.totalCount;
 
       let result = [];
@@ -141,18 +259,21 @@ export default {
       resData.pollDtlList.map((item, idx) => {
         let arr = [
           { isCheckbox: true },
-          { text: idx+1 },
+          { text: idx + 1 },
           { text: item.schulNm },
           { text: item.stGrade },
           { text: item.userNm },
-          { text: (item.sexdstn === 'F' ? '여' : '남') },
-          { text: item.brthy?.substring(0,4) + '-' + item.brthy?.substring(4,6)},
+          { text: item.sexdstn === 'F' ? '여' : '남' },
+          {
+            text:
+              item.brthy?.substring(0, 4) + '-' + item.brthy?.substring(4, 6),
+          },
           { text: 'test@gne.go.kr' },
-        ]
+        ];
 
         item.answer.map((subItem, index) => {
           arr.push({ text: subItem });
-          if(idx === 0) answerArr.push({ text: `EK1.${index+1}` });
+          if (idx === 0) answerArr.push({ text: `EK1.${index + 1}` });
         });
 
         result.push(arr);
@@ -162,15 +283,13 @@ export default {
       headerArr.value = answerArr;
       console.log(body.value);
       console.log(headerArr.value);
-    }
+    };
 
     const downloadExcel = () => {
       // if (infoArr.value.length) {
       //   const aEl = document.createElement('a');
-
       //   let text = '\uFEFF';
       //   text += titleArr.value.join(',') + '\n';
-
       //   infoArr.value.forEach((info) => {
       //     text += [info.name, info.email, info.grade, info.gender].join(',');
       //     if (selectedReport.value === all.value) {
@@ -186,14 +305,11 @@ export default {
       //     }
       //     text += '\n';
       //   });
-
       //   aEl.setAttribute(
       //     'href',
       //     `data:text/csv;charset=utf-8,${encodeURIComponent(text)}`
       //   );
-
       //   aEl.setAttribute('download', `${new Date()}_SEL.csv`);
-
       //   aEl.style.display = 'none';
       //   document.body.appendChild(aEl);
       //   aEl.click();
@@ -204,26 +320,42 @@ export default {
     };
 
     const goReportList = () => {
-      router.push({ name: 'IDManageReportView' })
-    }
+      router.push({ name: 'IDManageReportView' });
+    };
+
+    const searchSch = () => {
+      searchTextComplete.value = searchText.value;
+      fetchReportDtlList();
+    };
+
+    const updatePage = (v) => {
+      page.value = v;
+      fetchReportDtlList();
+    };
 
     return {
-      options,
-      selectedOption,
+      pollId,
+      options1,
+      selectedOption1,
       options2,
       selectedOption2,
+      updateText2,
       pollNm,
       header,
       headerArr,
       body,
-      pageNo,
+      page,
       recordCount,
       totalCount,
+      searchText,
 
       handleSelection1,
+      handleSelection2,
       fetchReportDtlList,
       downloadExcel,
-      goReportList
+      goReportList,
+      searchSch,
+      updatePage,
     };
   },
 };
