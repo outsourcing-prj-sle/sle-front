6dae1d42213f2b1c530a4aa690e00bef25e74e79
hwansoo <realyhs1@gmail.com>
2024-09-02 10:38:39 +0900

    2

--- src/views/NaverCallBackPage.vue
+++ src/views/NaverCallBackPage.vue
@@ -12,14 +12,14 @@ export default {
 
     if (code && state) {
       window.opener.postMessage({ type, code, state }, window.location.origin);
-      window.opener.postMessage(
-        { type, code, state },
-        process.env.VUE_APP_PRODUCTION === 'live' ? process.env.VUE_APP_REDIRECT_URI_LIVE : 
-        (process.env.VUE_APP_PRODUCTION === 'inner' ? process.env.VUE_APP_REDIRECT_URI_INNER_DEV :
-        (
-          process.env.VUE_APP_PRODUCTION === 'outer' ? process.env.VUE_APP_REDIRECT_URI_OUTER_DEV : process.env.VUE_APP_REDIRECT_URI_LOCAL
-        ))
-      );
+      // window.opener.postMessage(
+      //   { type, code, state },
+      //   process.env.VUE_APP_PRODUCTION === 'live' ? process.env.VUE_APP_REDIRECT_URI_LIVE :
+      //   (process.env.VUE_APP_PRODUCTION === 'inner' ? process.env.VUE_APP_REDIRECT_URI_INNER_DEV :
+      //   (
+      //     process.env.VUE_APP_PRODUCTION === 'outer' ? process.env.VUE_APP_REDIRECT_URI_OUTER_DEV : process.env.VUE_APP_REDIRECT_URI_LOCAL
+      //   ))
+      // );
       window.close();
     }
   },
